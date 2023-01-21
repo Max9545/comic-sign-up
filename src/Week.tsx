79e1cb21671e530a998653dbf10a7cd4ca599dae
Max Bregman <bregmanmax91@gmail.com>
2023-01-20 18:58:24 -0700

    attempt at CSS

--- src/Week.tsx
+++ src/Week.tsx
@@ -13,63 +13,61 @@ function Week(clubType: string, weeklyShowTimes: []) {
     setClub(clubType.clubType)
   })
 
-  // useEffect(() => {
-  //   setDays()
-  // })
-
     return (
       <>
         <h1>{`Sign up for ${club}`}</h1>
-        <Show 
-        day='monday' 
-        time='7:30'
-        pay={25}
-        />
-        <Show 
-        day='tuesday' 
-        time='6:00'
-        pay={40}
-        />
-        <Show 
-        day='tuesday' 
-        time='8:00'
-        pay={40}
-        />
-        <Show 
-        day='wednesday' 
-        time='7:30'
-        pay={40}
-        />
-        <Show 
-        day='thursday' 
-        time='7:30'
-        pay={50}
-        />
-        <Show 
-        day='friday' 
-        time='7:30'
-        pay={50}
-        />
-        <Show 
-        day='friday' 
-        time='9:30'
-        pay={40}
-        />
-        <Show 
-        day='saturday' 
-        time='7:30'
-        pay={40}
-        />
-        <Show 
-        day='saturday' 
-        time='9:30'
-        pay={50}
-        />
-        <Show 
-        day='sunday' 
-        time='5:30'
-        pay={60}
-        />
+        <div classname='show-container'>
+          <Show 
+          day='monday' 
+          time='7:30'
+          pay={25}
+          />
+          <Show 
+          day='tuesday' 
+          time='6:00'
+          pay={40}
+          />
+          <Show 
+          day='tuesday' 
+          time='8:00'
+          pay={40}
+          />
+          <Show 
+          day='wednesday' 
+          time='7:30'
+          pay={40}
+          />
+          <Show 
+          day='thursday' 
+          time='7:30'
+          pay={50}
+          />
+          <Show 
+          day='friday' 
+          time='7:30'
+          pay={50}
+          />
+          <Show 
+          day='friday' 
+          time='9:30'
+          pay={40}
+          />
+          <Show 
+          day='saturday' 
+          time='7:30'
+          pay={40}
+          />
+          <Show 
+          day='saturday' 
+          time='9:30'
+          pay={50}
+          />
+          <Show 
+          day='sunday' 
+          time='5:30'
+          pay={60}
+          />
+        </div>
       </>
     )
 }
