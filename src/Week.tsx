04fdfba1fec69c2ef06abf686d6bd0e01e0de8ac
Max Bregman <bregmanmax91@gmail.com>
2023-04-27 11:51:49 -0600

    Shorten reload time after comic submission

--- src/Week.tsx
+++ src/Week.tsx
@@ -75,7 +75,7 @@ function Week(props: {comedian: Comic, weeklyShowTimes: [ShowToBook]}) {
     alert('Availability Submitted!! Check your email and phone for verification of your latest availabilty')
     setTimeout(() => {
       window.location.reload()
-    }, 1000)
+    }, 500)
   }
 
     return (
