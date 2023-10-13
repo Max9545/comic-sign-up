433827d0d446123c71b267f7fd739bc9e98fd0e5
Max Bregman <bregmanmax91@gmail.com>
2023-10-13 10:18:26 -0600

    Make club names all capital letters on show divs

--- src/Show.tsx
+++ src/Show.tsx
@@ -106,7 +106,7 @@ function Show(props: {key: number, day: string, time: string, currentClub: strin
   return (
     <div className='show'>
     {props.headliner && <button onClick={(event) => handleClick(event)} 
-      className={`${availability} show-button`}>{`${props.day} (${props.date}) ${props.time} ${props.currentClub.charAt(0).toUpperCase() + props.currentClub.slice(1)} ${props.headliner}`}: 
+      className={`${availability} show-button`}>{`${props.day} (${props.date}) ${props.time} ${props.currentClub.toUpperCase()} ${props.headliner}`}: 
       <br></br>
       {availability   ? `Available` : `Not Available`}</button>}
   </div>
