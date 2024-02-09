aa88b7749e6651dc8712ebfd9b7dbdfcb621141e
Max Bregman <bregmanmax91@gmail.com>
2024-02-09 13:33:21 -0700

    SHow info displayed in correct places

--- src/ComediansGrid.tsx
+++ src/ComediansGrid.tsx
@@ -6,7 +6,7 @@ const ComediansGrid: React.FC<{ comedians: any[]; shows: any[] }> = ({ comedians
   // Group comedians by type
   const groupedComedians: { [type: string]: any[] } = {
     Pro: [],
-    OutOfTownPros: [],
+    OutOfTown: [],
     AlmostFamous: [],
     Inactive: []
   };
@@ -15,7 +15,7 @@ const ComediansGrid: React.FC<{ comedians: any[]; shows: any[] }> = ({ comedians
     if (comedian.comedianInfo.type === 'pro') {
       groupedComedians.Pro.push(comedian);
     } else if (comedian.comedianInfo.type === 'OutOfTown') {
-      groupedComedians.OutOfTownPros.push(comedian);
+      groupedComedians.OutOfTown.push(comedian);
     } else if (comedian.comedianInfo.type === 'AlmostFamous') {
       groupedComedians.AlmostFamous.push(comedian);
     } else if (comedian.comedianInfo.type === 'Inactive') {
@@ -33,54 +33,47 @@ const ComediansGrid: React.FC<{ comedians: any[]; shows: any[] }> = ({ comedians
         <div className="cell header">South</div>
       </div>
 
-      {/* Header for show information under "Pro" */}
-      {groupedComedians.Pro.length > 0 && (
-        <div className="row">
-          <div className="cell header" style={{ backgroundColor: '#ccc' }}>Pro</div>
-          {shows.filter(show => show.club === 'downtown').map((show) => (
-            <div className="cell header" key={show.id}>
-              {show.day} {show.time} {show.headliner} {show.club}
-            </div>
-          ))}
-          {shows.filter(show => show.club === 'south').map((show) => (
-            <div className="cell header" key={show.id}>
-              {show.day} {show.time} {show.headliner} {show.club}
-            </div>
-          ))}
-        </div>
-      )}
-
-      {/* Comedians section */}
-      {Object.entries(groupedComedians).map(([type, comediansOfType]) => (
-        <React.Fragment key={type}>
-          {/* Header for comedian type */}
-          <div className="row">
-            <div className="cell header" style={{ backgroundColor: '#ccc' }}>{type.replace(/([A-Z])/g, ' $1').trim()}</div>
-            <div className="cell"></div> {/* Empty cell for spacing */}
-          </div>
-          {/* Comedians of this type */}
-          {comediansOfType.map((comedian) => (
-            <div className="row" key={comedian.comedianInfo.id}>
-              <div className="cell">{comedian.comedianInfo.name}</div>
-              {/* Iterate through shows to determine if comedian is available */}
-              {shows.filter(show => show.club === 'downtown').map((show) => (
-                <div className="cell" key={`${comedian.comedianInfo.id}-${show.id}`}>
-                  {((comedian.comedianInfo.showsAvailabledowntown && comedian.comedianInfo.showsAvailabledowntown[show.day.toLowerCase()].includes(show.id)) ||
-                    (comedian.comedianInfo.showsAvailablesouth && comedian.comedianInfo.showsAvailablesouth[show.day.toLowerCase()].includes(show.id))) ? 
-                    'X' : ''}
-                </div>
+      {/* Iterate through shows to display show information for each Comic Type */}
+      {Object.entries(groupedComedians).map(([type, comediansOfType]) => {
+        return (
+          <React.Fragment key={type}>
+            {/* Header for Comic Type */}
+            <div className="row">
+              <div className="cell header" style={{ backgroundColor: '#ccc' }}>{type.replace(/([A-Z])/g, ' $1').trim()}</div>
+              {/* Show information for Downtown */}
+              {shows.map((show) => (
+                <React.Fragment key={`${type}-${show.id}-downtown`}>
+                  {show.club === 'downtown' && (
+                    <div className="cell">{`${show.day} ${show.time} ${show.headliner} ${show.club}`}</div>
+                  )}
+                </React.Fragment>
               ))}
-              {shows.filter(show => show.club === 'south').map((show) => (
-                <div className="cell" key={`${comedian.comedianInfo.id}-${show.id}`}>
-                  {((comedian.comedianInfo.showsAvailabledowntown && comedian.comedianInfo.showsAvailabledowntown[show.day.toLowerCase()].includes(show.id)) ||
-                    (comedian.comedianInfo.showsAvailablesouth && comedian.comedianInfo.showsAvailablesouth[show.day.toLowerCase()].includes(show.id))) ? 
-                    'X' : ''}
-                </div>
+              {/* Show information for South */}
+              {shows.map((show) => (
+                <React.Fragment key={`${type}-${show.id}-south`}>
+                  {show.club === 'south' && (
+                    <div className="cell">{`${show.day} ${show.time} ${show.headliner} ${show.club}`}</div>
+                  )}
+                </React.Fragment>
               ))}
             </div>
-          ))}
-        </React.Fragment>
-      ))}
+            {/* Comedians of this type */}
+            {comediansOfType.map((comedian) => (
+              <div className="row" key={comedian.comedianInfo.id}>
+                <div className="cell">{comedian.comedianInfo.name}</div>
+                {/* Iterate through shows to determine if comedian is available */}
+                {shows.map((show) => (
+                  <div className="cell" key={`${comedian.comedianInfo.id}-${show.id}`}>
+                    {((comedian.comedianInfo.showsAvailabledowntown && comedian.comedianInfo.showsAvailabledowntown[show.day.toLowerCase()].includes(show.id)) ||
+                      (comedian.comedianInfo.showsAvailablesouth && comedian.comedianInfo.showsAvailablesouth[show.day.toLowerCase()].includes(show.id))) ?
+                      'X' : ''}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
