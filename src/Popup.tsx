8ac0878e9fa137be331ab4e3720c3a7f9ce6b1c5
Max Bregman <bregmanmax91@gmail.com>
2024-02-21 14:46:09 -0700

    make popup dropdown into list of buttons

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -6,47 +6,44 @@ interface PopupProps {
 }
 
 const Popup: React.FC<PopupProps> = ({ position, onClose }) => {
-  const [selectedPosition, setSelectedPosition] = useState<string>('MC'); // Initialize selectedPosition state with an empty string
   const [otherPosition, setOtherPosition] = useState<string>('')
 
   // Function to handle position selection
-  const handlePositionSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedPosition(event.target.value);
+  const handlePositionSelection = (position: string) => {
+    onClose(position);
   };
 
   return (
-    
     <div className="popup" style={{ position: 'fixed', top: position.y, left: position.x }}
     onKeyUp={(e) => {
       if (e.key === "Enter") {
-        onClose(otherPosition || selectedPosition)    
+        onClose(otherPosition);    
       }
     }}
     >
     <div className="close-icon" onClick={() => onClose('')}>
-    <span>X</span>
-  </div>
-    <br></br>
-      <div className="popup-content">
+      <span>X</span>
+    </div>
+    <br />
+    <div className="popup-content">
+      <div>
+        <label>Position:</label>
         <div>
-          <label htmlFor="position">Position:</label>
-          <select id="position" onChange={handlePositionSelection} autoFocus>
-            <option value="MC">MC</option>
-            <option value="A1">A1</option>
-            <option value="B1">B1</option>
-            <option value="*7">*7</option>
-            <option value="*MC">*MC</option>
-            <option value='X'>X</option>
-            <option value='remove'>Remove</option>
-          </select>
+          <button onClick={() => handlePositionSelection('MC')}>MC</button>
+          <button onClick={() => handlePositionSelection('A1')}>A1</button>
+          <button onClick={() => handlePositionSelection('B1')}>B1</button>
+          <button onClick={() => handlePositionSelection('*7')}>*7</button>
+          <button onClick={() => handlePositionSelection('*MC')}>*MC</button>
+          <button onClick={() => handlePositionSelection('X')}>X</button>
+          <button onClick={() => handlePositionSelection('remove')}>Remove</button>
         </div>
-        <div>
-          <label htmlFor="other">Other:</label>
-          <input type="text" id="other" onChange={(e) => setOtherPosition(e.target.value)}/>
-        </div>
-        <button onClick={() => onClose(otherPosition || selectedPosition)}>Submit</button> {/* Pass the selected position to onClose function */}
+      </div>
+      <div>
+        <label htmlFor="other">Other:</label>
+        <input type="text" id="other" onChange={(e) => setOtherPosition(e.target.value)}/>
       </div>
     </div>
+  </div>
   );
 };
 
